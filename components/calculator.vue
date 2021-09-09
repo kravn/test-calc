9d0be920d5b0fa113894fcfb662a6e638590fa77
Karl Fabe <karl@aloware.com>
2021-09-09 10:06:17 +0800

    Partial update

---
+++ components/calculator.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-card
+    light flat
+    width="800"
+    class="pa-0">
+    <v-card class="calc-screen pa-3" outlined>
+      <v-card-actions class="pa-0">
+        <v-spacer></v-spacer>
+        <div v-if="answer">
+          Ans = {{ answer }}
+        </div>
+        <div v-else>&nbsp;</div>
+      </v-card-actions>
+      <v-card-actions class="pa-0">
+        <v-spacer></v-spacer>
+        <div class="display-1">
+          {{ strValue }}
+        </div>
+      </v-card-actions>
+    </v-card>
+    <template 
+      v-for="(n, key) in calcBtns">
+      <div
+        v-if="n.active"
+        @click="collect(n.key, n.multi)"
+        :key="key"
+        :class="`btn--calculator ${n.color}`">
+        {{ n.label }}
+      </div>
+      <div
+        v-else
+        :key="key"
+        :class="`btn--calculator ${n.color}`">
+        {{ n.label }}
+      </div>
+    </template>
+  </v-card>
+</template>
+
+<script>
+
+  import { CALCULATOR_BUTTONS } from '~/components/constants/calculator.js'
+  import Calcu from '@mroutput/jscalc'
+
+  const btns = { CALCULATOR_BUTTONS }
+
+  export default {
+    name: 'Calculator',
+    computed: {
+      calcBtns () {
+        return btns.CALCULATOR_BUTTONS
+      }
+    },
+    methods: {
+      collect (val, multi = false) {
+        var c = new Calcu()
+
+        if (this.strValue === 0 || this.strValue === '0') {
+          this.strValue = ''
+        }
+
+        if (multi) {
+          if (val === '=') {
+            try {
+              this.answer = c.exec(this.strValue)
+            }
+            catch(e) {
+              this.answer = e
+            }
+          }
+          else if (val === 'ce') {
+            this.strValue = '0'
+            this.answer = null
+          } else {
+            this.flag = false
+            this.strValue += `${val}`
+          }
+        }
+        else {
+          if (!this.flag) {
+            this.strValue += `${val}`
+            this.flag = true
+          }
+          
+        }
+                
+      }
+    },
+    data() {
+      return {
+        strValue: '0',
+        answer: null,
+        flag: false
+      }
+    }
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container class="text-center" >
+    <!-- <v-row justify="center" align="center">
+      <v-col cols="12" sm="8" md="6"> -->
+        <v-card outlined min-width="832">
+          <v-card-text class="headline py-12">
+            <p class="text-center black--text font-weight-bold">Tech Challenge - Calculator</p>
+          </v-card-text>
+          <v-card-actions class="pb-10">
+            <v-spacer></v-spacer>
+            <CalculatorUI />
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+      <!-- </v-col>
+    </v-row> -->
+  </v-container>
+</template>
+
+<script>
+
+  import CalculatorUI from '~/components/calculator'
+
+  export default {
+    name: 'Calculator',
+    components: { CalculatorUI }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
